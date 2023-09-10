e72dbb4867db582d0e11c7a4ae42037e8d5f62d1
harvysdlc <harvysdlc@gmail.com>
2023-09-10 13:21:08 +0800

    css

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>meeps!</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="../meeps/css/index.css">
     <link rel="icon" type="image/png" href="../meeps/images/logo.png">
 </head>
 <body>
