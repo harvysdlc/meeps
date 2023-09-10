cadd290dc0b0cdf7068ea644b3a4ceab0fc3847f
harvysdlc <harvysdlc@gmail.com>
2023-09-10 14:05:22 +0800

    config etc

---
+++ signup.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup - meeps!</title>
+    <link rel="stylesheet" href="../meeps/css/signup.css">
+    <link rel="icon" type="image/png" href="../meeps/images/logo.png">
+</head>
+<body>
+    <h2> Signup </h2>
+    <form class="" action="" method="post" autocomplete="off">
+        <label for="name"> Name: </label>
+        <input type="text" name="name" id="name" required value="" <br>
+        <label for="username"> Userame: </label>
+        <input type="text" name="username" id="username" required value="" <br>
+        <label for="email"> Email: </label>
+        <input type="text" name="email" id="email" required value="" <br>
+        <label for="password"> Password: </label>
+        <input type="text" name="password" id="password" required value="" <br>
+        <label for="confirmpassword"> Confirm Password: </label>
+        <input type="password" name="confirmpassword" id="confirmpassword" required value="" <br>
+        <button type="submit" name="submit"> Sign up </button>
+    </form>
+</body>
+</html>
