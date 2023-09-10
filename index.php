f84cf64e82881d23910cf73c512444476bc197f0
harvysdlc <harvysdlc@gmail.com>
2023-09-10 15:15:39 +0800

    id

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require 'config.php';
 if(!empty($_SESSION["id"])){
     $id = $_SESSION["id"];
-    $result = mysqli_query($conn, "SELECT * FROM tb_user WHERE id = $id");
+    $result = mysqli_query($conn, "SELECT * FROM tb_user WHERE id = '$id'");
     $row = mysqli_fetch_assoc($result);
 }
 else{
