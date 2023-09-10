4ad2456b081b852a2700ca0763226a4ff48cf8a0
harvysdlc <harvysdlc@gmail.com>
2023-09-10 14:06:53 +0800

    brackets

--- signup.php
+++ signup.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Signup - meeps!</title>
+    <title>Sign up - meeps!</title>
     <link rel="stylesheet" href="../meeps/css/signup.css">
     <link rel="icon" type="image/png" href="../meeps/images/logo.png">
 </head>
@@ -11,15 +11,15 @@
     <h2> Signup </h2>
     <form class="" action="" method="post" autocomplete="off">
         <label for="name"> Name: </label>
-        <input type="text" name="name" id="name" required value="" <br>
+        <input type="text" name="name" id="name" required value=""> <br>
         <label for="username"> Userame: </label>
-        <input type="text" name="username" id="username" required value="" <br>
+        <input type="text" name="username" id="username" required value=""> <br>
         <label for="email"> Email: </label>
-        <input type="text" name="email" id="email" required value="" <br>
+        <input type="text" name="email" id="email" required value=""> <br>
         <label for="password"> Password: </label>
-        <input type="text" name="password" id="password" required value="" <br>
+        <input type="text" name="password" id="password" required value=""> <br>
         <label for="confirmpassword"> Confirm Password: </label>
-        <input type="password" name="confirmpassword" id="confirmpassword" required value="" <br>
+        <input type="password" name="confirmpassword" id="confirmpassword" required value=""> <br>
         <button type="submit" name="submit"> Sign up </button>
     </form>
 </body>
