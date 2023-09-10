bab3f86f6a6e0b80e896682be6489ed2745e84a4
harvysdlc <harvysdlc@gmail.com>
2023-09-10 14:28:02 +0800

    signup

--- signup.php
+++ signup.php
@@ -1,3 +1,32 @@
+<?php
+require 'config.php';
+
+if(isset($_POST["submit"])){
+    $name = $_POST["name"];
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $confirmpassword = $_POST["confirmpassword"];
+    $duplicate = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$username' OR email = '$email'");
+    if(mysqli_num_rows($duplicate)>0){
+        echo
+        "<script> alert('Username or Email has already been taken'); </script>";
+    }
+    else{
+        if($password == $confirmpassword){
+            $query = "INSERT INTO tb_user VALUES ('', '$name', '$username', '$email', '$passsword')";
+            mysqli_query($conn, $query);
+            echo
+            "<script> alert('Account Sign up Complete!'); </script>";
+        }
+        else{
+            echo
+            "<script> alert('Password does not match!'); </script>";
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,5 +51,7 @@
         <input type="password" name="confirmpassword" id="confirmpassword" required value=""> <br>
         <button type="submit" name="submit"> Sign up </button>
     </form>
+    <br>
+    <a href="signin.php">Sign in </a>
 </body>
 </html>
