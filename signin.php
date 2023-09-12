eba2e7ddf671b9c076b4c0f8cd6a44ce1730e37c
harvysdlc <harvysdlc@gmail.com>
2023-09-12 19:56:20 +0800

    inpur

--- signin.php
+++ signin.php
@@ -31,7 +31,7 @@ if(isset($_POST["submit"])){
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
     <title>Sign in - meeps!</title>
     <link rel="stylesheet" href="../meeps/css/signin.css"> 
     <link rel="icon" type="image/png" href="../meeps/images/logo.png">
