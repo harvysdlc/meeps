75cb3016f4db88cc827d532ee881e2723cb74129
harvysdlc <harvysdlc@gmail.com>
2023-09-14 19:21:33 +0800

    mutuals

--- mutuals.php
+++ mutuals.php
@@ -16,26 +16,29 @@ else{
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>meeps!</title>
-    <link rel="stylesheet" href="../meeps/css/index.css">
+    <link rel="stylesheet" href="../meeps/css/search.css">
     <link rel="icon" type="image/png" href="../meeps/images/logo.png">
 </head>
 <body>
     <div class="rectangle"> </div>
     <div class="navigation">
-        <button onclick="window.location.href='../meeps/index.php'">Home</button>
-        <button onclick="window.location.href=''">Search</button>
-        <button onclick="window.location.href=''">Mutuals</button>
-        <button onclick="window.location.href=''">Beeps</button>
-        <button onclick="window.location.href=''">Profile</button>
+    <button onclick="window.location.href='../meeps/index.php'">Home</button>
+        <button onclick="window.location.href='../meeps/search.php'">Search</button>
+        <button onclick="window.location.href='../meeps/mutuals.php'">Mutuals</button>
+        <button onclick="window.location.href='../meeps/beeps.php'">Beeps</button>
+        <button onclick="window.location.href='../meeps/profile.php'">Profile</button>
     </div>
     <div class="logo">
         <img src="../meeps/images/logowhite.png">
     </div>
     <div class="welcome">
-        <h2> Welcome <?php echo $row["username"]; ?> </h2>
+        <h2> <?php echo $row["username"]; ?> </h2>
     </div>
     <div class="signout">
         <a href="signout.php"> Sign out </a>
+    </div>
+    <div class="triangle">
+        <img src="../meeps/images/triangle.png">
     </div>
     <div class="grayrectangle"> </div>
 </body>
