2d84b20ec3511ef08491c8d19094aa8d696b416d
harvysdlc <harvysdlc@gmail.com>
2023-09-14 18:45:52 +0800

    search

---
+++ registered/beeps.php
@@ -0,0 +1,42 @@
+<?php
+require 'config.php';
+if(!empty($_SESSION["id"])){
+    $id = $_SESSION["id"];
+    $result = mysqli_query($conn, "SELECT * FROM tb_user WHERE id = '$id'");
+    $row = mysqli_fetch_assoc($result);
+}
+else{
+    header("Location: signin.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>meeps!</title>
+    <link rel="stylesheet" href="../meeps/css/index.css">
+    <link rel="icon" type="image/png" href="../meeps/images/logo.png">
+</head>
+<body>
+    <div class="rectangle"> </div>
+    <div class="navigation">
+        <button onclick="window.location.href='../meeps/index.php'">Home</button>
+        <button onclick="window.location.href=''">Search</button>
+        <button onclick="window.location.href=''">Mutuals</button>
+        <button onclick="window.location.href=''">Beeps</button>
+        <button onclick="window.location.href=''">Profile</button>
+    </div>
+    <div class="logo">
+        <img src="../meeps/images/logowhite.png">
+    </div>
+    <div class="welcome">
+        <h2> Welcome <?php echo $row["username"]; ?> </h2>
+    </div>
+    <div class="signout">
+        <a href="signout.php"> Sign out </a>
+    </div>
+    <div class="grayrectangle"> </div>
+</body>
+</html>
