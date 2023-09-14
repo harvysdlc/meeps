2d178e6a7f8d4fc6c9f628f7513555ec8e93bc7a
harvysdlc <harvysdlc@gmail.com>
2023-09-14 19:31:51 +0800

    pages

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ else{
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>meeps!</title>
+    <title>Home - meeps!</title>
     <link rel="stylesheet" href="../meeps/css/index.css">
     <link rel="icon" type="image/png" href="../meeps/images/logo.png">
 </head>
