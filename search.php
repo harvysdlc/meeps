8acdc457c1ec7666979496f0a32f7c12f11c42c2
harvysdlc <harvysdlc@gmail.com>
2023-09-16 22:11:42 +0800

    ??

--- search.php
+++ search.php
@@ -12,7 +12,7 @@ else{
 if(isset($_POST['searchbutton'])){
     $search = $_POST['search'];
     $searchquery = mysqli_query($conn, "SELECT * FROM tb_user WHERE username LIKE '%$search%'"); 
-    $searchresult = mysqli_fetch_assoc($searchquery);
+    $searchresult = mysqli_fetch_array($searchquery);
 }
 
 else if(!isset($_POST['searchbutton'])){
