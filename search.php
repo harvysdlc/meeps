9f6aaa20b2eff2a002dd4bc38ea19bfac8fdb1bc
harvysdlc <harvysdlc@gmail.com>
2023-09-17 13:01:09 +0800

    or

--- search.php
+++ search.php
@@ -11,7 +11,7 @@ else{
 
 if(isset($_POST['searchbutton'])){
     $search = $_POST['search'];
-    $searchquery = mysqli_query($conn, "SELECT * FROM tb_user WHERE name LIKE '%$search%'"); 
+    $searchquery = mysqli_query($conn, "SELECT * FROM tb_user WHERE name, OR username LIKE '%$search%'"); 
     $searchresult = mysqli_fetch_assoc($searchquery);
 }
 
